489ff4e68afff3f17373990bc044a977d7eb6fa3
Teidata <spa@teidata.com>
2016-10-18 18:14:25 +0100

    Update index2.js

--- index2.js
+++ index2.js
@@ -9,10 +9,12 @@ $(document).ready(function() {
     fade: true,
   }
     );
+});
   /* Formato del slider */
 $(document).ready(function() {
   $('.ba-slider').beforeAfter();
-  // Configuramos la librería jQuery Accordion
+});
+// Configuramos la librería jQuery Accordion
 $('.accordion').accordion({
   transitionSpeed: 400,        // velocidad del acordeón
   transitionEasing: 'ease'    // ritmo del acordeón (linear, ease-in…)
